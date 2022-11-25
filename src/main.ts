2c7ab70a4882c92d818b8bfeede81aed20f2c1dd
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-25 18:11:59 +0200

    backend <> add global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 (async () => {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    })
+  );
   await app.listen(process.env.PORT);
-  console.info(`SERVER IS RUNNING ON PORT ${process.env.PORT}`);
+  console.info(`SERVER IS RUNNING ON PORT: ${process.env.PORT}`);
 })();
