871e4cf63b880783b368e5405729db3b07d1ccd3
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-25 17:17:50 +0200

    backend <> add env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+(async () => {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(process.env.PORT);
+  console.info(`SERVER IS RUNNING ON PORT ${process.env.PORT}`);
+})();
