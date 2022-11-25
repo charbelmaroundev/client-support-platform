412044cf68f2eb1d169d612638facba48162395c
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-25 18:12:16 +0200

    backend <> add user shema

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,1 +1,37 @@
-export class SignUpDto {}
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+export class SignUpDto {
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @MinLength(8)
+  @IsString()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isVIP: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  isAdmin: boolean;
+}
