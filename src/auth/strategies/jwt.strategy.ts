a7dc14d9baadd54467d1125e0807b91a3e03cf2a
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-26 08:55:56 +0200

    backend <> add jwt strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.SECRET_KEY_JWT,
+    });
+  }
+
+  async validate(payload: any) {
+    const id = payload.id;
+
+    console.log(id);
+
+    const user = await this.authService.checkUser(id);
+    console.log(user);
+
+    if (!user) throw new UnauthorizedException();
+
+    return { id };
+  }
+}
