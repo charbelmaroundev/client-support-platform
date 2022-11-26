2d0951b18a83527fb3dd48065d9360b14b75e4b5
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-26 08:57:59 +0200

    backend <> add config and jwt modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schemas/user.schema';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.SECRET_KEY_JWT,
+      signOptions: { expiresIn: process.env.EXPIRES_IN_JWT },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
