e742ab2049c906a18c93d957f0f2a13b29b1863f
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-26 08:55:40 +0200

    backend <> add signin controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/signin.dto';
 import { SignUpDto } from './dto/signup.dto';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('signup')
   signup(@Body() body: SignUpDto) {
     return this.authService.signup(body);
+  }
+
+  @Public()
+  @Post('signin')
+  signin(@Body() body: SignInDto) {
+    return this.authService.signin(body);
+  }
+
+  @Get()
+  test() {
+    console.log('TEST');
   }
 }
