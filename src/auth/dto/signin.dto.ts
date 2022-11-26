4d24784cc32611d7bb48f6c8697407385151ab61
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-26 08:57:21 +0200

    backend <> add signin dto

---
+++ src/auth/dto/signin.dto.ts
@@ -0,0 +1,21 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+export class SignInDto {
+  @IsEmail()
+  readonly email: string;
+
+  @MinLength(8)
+  @IsString()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  readonly password: string;
+}
