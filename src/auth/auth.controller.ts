a986368896f8d291e8e04793db401225f39b625d
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-26 09:29:21 +0200

    backend <> add serialize for sign up, sign in and who am i

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,9 @@ import { SignUpDto } from './dto/signup.dto';
 import { Public } from './decorators/public.decorator';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './schemas/user.schema';
+import { Serialize } from '../interceptor/serialize.interceptor';
+import { UserDto } from './dto/user.dto';
+import { WhoAmIDto } from './dto/who-am-i.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,17 +15,20 @@ export class AuthController {
 
   @Public()
   @Post('signup')
+  @Serialize(UserDto)
   signup(@Body() body: SignUpDto) {
     return this.authService.signup(body);
   }
 
   @Public()
   @Post('signin')
+  @Serialize(UserDto)
   signin(@Body() body: SignInDto) {
     return this.authService.signin(body);
   }
 
   @Get('me')
+  @Serialize(WhoAmIDto)
   whoami(@CurrentUser() user: User) {
     return this.authService.whoami(user.id);
   }
