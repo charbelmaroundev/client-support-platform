833efbd0ee16e28fd3cbb5de383a90be3bcf45a4
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-28 10:29:31 +0200

    backend <> add objectId dto

---
+++ src/complaint/dto/objectId.dto.ts
@@ -0,0 +1,7 @@
+import { ObjectId } from 'mongoose';
+import { IsObjectId } from 'class-validator-mongo-object-id';
+
+export class ObjectIdDto {
+  @IsObjectId({ message: 'Param should be an objectId' })
+  id: ObjectId;
+}
