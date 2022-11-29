6c86535e2220dc972e892f18ac53042ce58629a0
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-29 15:06:59 +0200

    backend <> add transform user and who am i dtos

--- src/auth/dto/user.dto.ts
+++ src/auth/dto/user.dto.ts
@@ -1,12 +1,21 @@
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 
 export class UserDto {
   @Expose()
+  @Transform(
+    (firstName) =>
+      firstName.value.charAt(0).toUpperCase() + firstName.value.slice(1)
+  )
   readonly firstName: string;
 
   @Expose()
+  @Transform(
+    (lastName) =>
+      lastName.value.charAt(0).toUpperCase() + lastName.value.slice(1)
+  )
   readonly lastName: string;
 
   @Expose()
+  @Transform((email) => email.value.toLowerCase())
   readonly email: string;
 }
--- src/auth/dto/who-am-i.dto.ts
+++ src/auth/dto/who-am-i.dto.ts
@@ -1,4 +1,4 @@
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 import { ObjectId } from 'mongoose';
 
 export class WhoAmIDto {
@@ -6,12 +6,21 @@ export class WhoAmIDto {
   readonly id: ObjectId;
 
   @Expose()
+  @Transform(
+    (firstName) =>
+      firstName.value.charAt(0).toUpperCase() + firstName.value.slice(1)
+  )
   readonly firstName: string;
 
   @Expose()
+  @Transform(
+    (lastName) =>
+      lastName.value.charAt(0).toUpperCase() + lastName.value.slice(1)
+  )
   readonly lastName: string;
 
   @Expose()
+  @Transform((email) => email.value.toLowerCase())
   readonly email: string;
 
   @Expose()
