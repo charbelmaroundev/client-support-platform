14994a76e694cd7f86a734858dbbbf0aeb5e5a6b
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-29 15:07:38 +0200

    backend <> fix bugs in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,15 +10,18 @@ import { SignUpDto } from './dto/signup.dto';
 import * as bcrypt from 'bcrypt';
 import { SignInDto } from './dto/signin.dto';
 import { JwtService } from '@nestjs/jwt';
-import { UnauthorizedException } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
-import { TokenDto } from './dto/token.dto';
+import { hashData } from '../utils/hash-data..util';
+import { UserService } from '../user/user.service';
+import { Payload } from 'src/types/index.type';
+import { AccessToken } from 'src/types/index.type';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly jwtService: JwtService,
-    @InjectModel('user') private readonly userModel: Model<User>
+    private readonly userService: UserService,
+    @InjectModel('User') private readonly userModel: Model<User>
   ) {}
 
   async signup(body: SignUpDto): Promise<UserDto> {
@@ -38,12 +41,14 @@ export class AuthService {
       isVIP: boolean;
     } = body;
 
-    const checkEmail: User | null = await this.checkUserByEmail(email);
+    const checkEmail: User | null = await this.userService.checkUserByEmail(
+      email
+    );
 
     if (checkEmail)
       throw new ConflictException('User with such email already exist');
 
-    const hashedPassword: string = await this.hashData(password);
+    const hashedPassword: string = await hashData(password);
 
     const user: User = await this.userModel.create({
       firstName,
@@ -57,9 +62,11 @@ export class AuthService {
     return user;
   }
 
-  async signin(body: SignInDto): Promise<TokenDto> {
+  async signin(body: SignInDto): Promise<AccessToken> {
     const { email, password }: { email: string; password: string } = body;
-    const checkEmail: User | null = await this.checkUserByEmail(email);
+    const checkEmail: User | null = await this.userService.checkUserByEmail(
+      email
+    );
 
     if (!checkEmail) this.CredentialIncorrect();
 
@@ -70,7 +77,7 @@ export class AuthService {
 
     if (!isMatch) this.CredentialIncorrect();
 
-    const payload: object = { id: checkEmail.id };
+    const payload: Payload = { id: checkEmail.id };
 
     return {
       access_token: this.jwtService.sign(payload),
@@ -78,33 +85,12 @@ export class AuthService {
   }
 
   async whoami(id: string): Promise<User> {
-    const user: User = await this.checkUser(id);
+    const user: User = await this.userService.checkUserById(id);
 
     return user;
   }
 
   CredentialIncorrect() {
-    throw new ForbiddenException('Credentials incorrect');
-  }
-
-  async hashData(data: string): Promise<string> {
-    const salt: string = await bcrypt.genSalt();
-    const hash: string = await bcrypt.hash(data, salt);
-
-    return hash;
-  }
-
-  async checkUser(id: string): Promise<User> {
-    const user: User = await this.userModel.findById(id);
-
-    if (!user) throw new UnauthorizedException();
-
-    return user;
-  }
-
-  async checkUserByEmail(email: string): Promise<User | null> {
-    const user = await this.userModel.findOne({ email });
-
-    return user;
+    throw new ForbiddenException('Incorrect credentials!');
   }
 }
