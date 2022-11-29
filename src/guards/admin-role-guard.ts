e234c791665c8490f8004d6028a63986f6e29cb5
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-29 15:03:41 +0200

    backend <> add client and admin role guard

---
+++ src/guards/admin-role-guard.ts
@@ -0,0 +1,24 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  constructor(private readonly userService: UserService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    let user = request.user;
+
+    user = await this.userService.checkUserById(user.id);
+
+    if (user.isAdmin === true) return true;
+
+    throw new HttpException('UNAUTHORIZED ACCESS', HttpStatus.FORBIDDEN);
+  }
+}
