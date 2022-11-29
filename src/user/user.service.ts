81fb438125ec5ec9247cbba568ba8b4d7ded095c
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-11-29 15:01:24 +0200

    backend <> check user functions

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,41 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async checkUserById(id: string): Promise<User> {
+    const user: User = await this.userModel.findById(id);
+    if (!user) throw new UnauthorizedException();
+    return user;
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async checkUserByEmail(email: string): Promise<User | null> {
+    const user = await this.userModel.findOne({ email });
+
+    return user;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+  // create(createUserDto: CreateUserDto) {
+  //   return 'This action adds a new user';
+  // }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // findAll() {
+  //   return `This action returns all user`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} user`;
+  // }
+
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
 }
