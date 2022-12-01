c19d6728e52fdd4d7152a76612e2374db7c1ab29
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-12-01 14:53:16 +0200

    backend <> comment signin dto

--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,13 +1,13 @@
-import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
+// import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
 
-export class SignInDto {
-  @IsEmail()
-  readonly email: string;
+// export class SignInDto {
+//   @IsEmail()
+//   readonly email: string;
 
-  @MinLength(8)
-  @IsString()
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'Credentials incorrect',
-  })
-  readonly password: string;
-}
+//   @MinLength(8)
+//   @IsString()
+//   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+//     message: 'Credentials incorrect',
+//   })
+//   readonly password: string;
+// }
