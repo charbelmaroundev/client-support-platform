9874e7c51262a2921a335b99317d311b0bceab6c
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-12-01 14:48:08 +0200

    backend <> add update user dto

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,9 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsEnum } from 'class-validator';
+import { UpDowngrade } from '../../types/index.type';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class MethodDto {
+  @IsEnum(UpDowngrade, {
+    message: `Method should be ${UpDowngrade.UPGRADE} or ${UpDowngrade.DOWNGRADE}!`,
+  })
+  readonly method: UpDowngrade;
+}
